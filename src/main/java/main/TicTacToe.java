40b6bfde7fe6cec099ed79674ccffc620b12fc10
ranji <ranji@DESKTOP-8QJ3EPT>
2019-04-05 00:43:12 +0530

    main code

--- src/main/java/main/TicTacToe.java
+++ src/main/java/main/TicTacToe.java
@@ -5,18 +5,68 @@ import java.util.*;
 public class TicTacToe{
 public static int[][] ticbox;
 	public static int win(int [][]tic) {
+		int sum=0;
+		boolean flag=true;
 		//check all rows
 		//loop through rows from 0 to 3 and check if all the 3 places have same marks
- 		
+ 		for(int i=0;i<3;i++)
+ 		{
+ 			sum=0;
+ 			flag=true;
+ 			for(int j=0;j<3;j++)
+ 			{
+ 				if(tic[i][j]==0)
+ 					flag=false;
+ 					sum+=tic[i][j];
+ 			}
+ 			if((sum==3 || sum==6) && flag)
+ 				return tic[i][0];
+ 		}
 		
 		//check all cols
 		//loop through columns from 0 to 3 and check if all the 3 places have same marks
+ 		for(int i=0;i<3;i++)
+ 		{
+ 			sum=0;
+ 			flag=true;  		
+ 			for(int j=0;j<3;j++)
+ 			{ 
+ 				if(tic[j][i]==0)
+ 					flag=false;
+ 					sum+=tic[j][i];
+ 			}
+ 			if((sum==3 || sum==6)&& flag)
+ 				return tic[0][i];
+ 		}
+		
  		
-		//check both diagonals 
-		
-		
-		
-		//write your code here !!!
+ 		//check both diagonals 
+ 	    sum=0;
+ 	   flag=true;
+ 		for(int i=0;i<3;i++)
+ 		{
+ 			if(tic[i][i]==0)
+					flag=false;
+ 					sum+=tic[i][i];
+ 		}
+ 	   if((sum==3 || sum==6) && flag)
+			return tic[0][0];
+ 		
+ 			
+ 	    sum=0;
+ 	   flag=true;
+ 	    int j=0,k=2;
+ 		for(int i=0;i<3;i++)
+ 		{
+ 			if(tic[i][j]==0)
+				flag=false;
+ 			
+ 			sum+=tic[j++][k--];
+ 			
+ 		}		
+ 		 if((sum==3 || sum==6) && flag)
+ 			return tic[0][2];
+ 		
 		
 		
 		
@@ -35,13 +85,11 @@ public static int[][] ticbox;
 	public static boolean validate(int a1,int a2,int [][] tic) {
 		//check if a1 is between 0 & 3
 		//check if a2 is between 0 & 3
-		//check if the selected box is empty ie, already not marked by other player
-		//if all checks passed return true.
-		
-		
-		//Write your code here !!!
-		
-		
+    	//check if the selected box is empty ie, already not marked by other player
+		if(a1>=0 && a1<3)
+			if(a2>=0 && a2<3)
+				if(tic[a1][a2]==0)
+					return true;
 		return false;
 	}
 	public static void main(String args[]) {
